f6c0c0261827ccd81fc8b4c30b0aff6061441c2b
Seth <sbatterby@gmail.com>
2015-01-15 14:42:46 +0000

    Update license

--- js/tasks/FindNearestTask.js
+++ js/tasks/FindNearestTask.js
@@ -1,5 +1,20 @@
 /*global pulse, app, jQuery, require, document, esri, esriuk, Handlebars, console, $, mynearest, window, alert, unescape, define */
 
+/*
+ | Copyright 2015 ESRI (UK) Limited
+ |
+ | Licensed under the Apache License, Version 2.0 (the "License");
+ | you may not use this file except in compliance with the License.
+ | You may obtain a copy of the License at
+ |
+ |    http://www.apache.org/licenses/LICENSE-2.0
+ |
+ | Unless required by applicable law or agreed to in writing, software
+ | distributed under the License is distributed on an "AS IS" BASIS,
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ | See the License for the specific language governing permissions and
+ | limitations under the License.
+ */
 /* This task uses code from www.movable-type.co.uk                                                */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 /*  Latitude/longitude spherical geodesy formulae & scripts           (c) Chris Veness 2002-2014  */
