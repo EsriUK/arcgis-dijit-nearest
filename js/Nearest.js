e8b331d1d48f4ec7bf1fe4204e194b834f9b4afa
Seth <sbatterby@gmail.com>
2015-01-21 10:13:28 +0000

    Fix for query timeout

--- js/Nearest.js
+++ js/Nearest.js
@@ -185,7 +185,7 @@ function (
                                 layerOpts = _this._getlayerOptions(queryResults[j].itemId);
 
                                 // Perform find nearest on each set of features
-                                if ((queryResults[j].error === null) && (queryResults[j].results.features.length > 0)) {
+                                if ((queryResults[j].error === null) && (queryResults[j].results) && (queryResults[j].results.features.length > 0)) {
 
                                     nearestTask = new ClientNearestTask({
                                         maxResults: layerOpts.maxResults,
