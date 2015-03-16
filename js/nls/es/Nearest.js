4bd6a745fe7a480353e964e35c8204802ca28cd4
Seth <sbatterby@gmail.com>
2015-03-16 11:28:19 +0000

    Add in Spanish text

---
+++ js/nls/es/Nearest.js
@@ -0,0 +1,11 @@
+﻿define(
+    ({
+        noresults: "No se encontraron resultados",
+        showingclosest: "Mostrando lo más cercano",
+        within: "dentro",
+        showOnMapLinktext: "Mostrar en el mapa",
+        distanceUnits: "millas",
+        summaryDetailsTitle: "Alterna para visualizar los campos de las características ",
+        showOnMapTitleText: "Mostrar esta característica en el mapa"
+    })
+);
